af93c9d4d641bc98a2543e93398f493fe1e1fb5f
Razi Rais <razibinrais@outlook.com>
2017-05-20 13:11:33 -0400

    Update installdocker.ps1

--- reference-material/installdocker.ps1
+++ reference-material/installdocker.ps1
@@ -1,2 +1,1 @@
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider
+powershell -ExecutionPolicy Unrestricted Install-Module -Name DockerMsftProvider -Repository PSGallery -Force; powershell -ExecutionPolicy Unrestricted Install-Package -Name docker -ProviderName DockerMsftProvider -Force
