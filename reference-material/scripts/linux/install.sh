64e4d3333b4c49cd3389237c091df4a05c2c6704
Razi Rais <razibinrais@outlook.com>
2017-05-27 18:23:06 -0400

    Create install.sh

---
+++ reference-material/scripts/linux/install.sh
@@ -0,0 +1,18 @@
+sudo mkdir install-scripts
+cd install-scripts
+
+sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-docker.sh
+sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-nodejs.sh
+sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-yeoman.sh
+sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-dotnetcore.sh
+
+sudo chmod +x $PWD/install-docker.sh
+sudo chmod +x $PWD/install-nodejs.sh
+sudo chmod +x $PWD/install-yeoman.sh
+sudo chmod +x $PWD/install-dotnetcore.sh
+
+
+
+
+
+
