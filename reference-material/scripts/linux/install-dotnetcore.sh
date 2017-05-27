e616c10a40f639dc3d52ffd51d36bf143ff50394
Razi Rais <razibinrais@outlook.com>
2017-05-27 18:24:57 -0400

    Create install-dotnetcore.sh

---
+++ reference-material/scripts/linux/install-dotnetcore.sh
@@ -0,0 +1,8 @@
+#(Ubuntu 16.04) https://www.microsoft.com/net/core#linuxubuntu
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+
+sudo apt-get update
+
+sudo apt-get -y install dotnet-dev-1.0.0-preview2.1-003177
