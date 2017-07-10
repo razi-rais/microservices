3b5d8972fd6592ce6b2fc99d8da27992d324d3a7
Razi Rais <razibinrais@outlook.com>
2017-07-10 16:52:15 -0400

    Delete install-dotnet-1.1.0-runtime.sh

--- reference-material/install-dotnet-1.1.0-runtime.sh
+++
@@ -1,8 +0,0 @@
-
-#https://github.com/dotnet/dotnet-docker/blob/master/1.1/runtime/jessie/Dockerfile
-sudo curl -SL "https://dotnetcli.blob.core.windows.net/dotnet/release/1.1.0/Binaries/1.1.2/dotnet-ubuntu-x64.1.1.2.tar.gz" --output dotnet.tar.gz \
-    && sudo mkdir -p /usr/share/dotnet \
-    && sudo tar -zxf dotnet.tar.gz -C /usr/share/dotnet \
-    && sudo rm dotnet.tar.gz \
-    && sudo ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
-
