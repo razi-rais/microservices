afe71f752af65b6c16825df69e1cee80f3fc4131
Razi Rais <razibinrais@outlook.com>
2017-07-10 17:35:42 -0400

    Update install-dotnetcore.sh

--- reference-material/scripts/linux/install-dotnetcore.sh
+++ reference-material/scripts/linux/install-dotnetcore.sh
@@ -3,6 +3,6 @@ sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotne
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 
-sudo apt-get update
+sudo apt-get -y update
 
-sudo apt-get install dotnet-dev-1.0.4
+sudo apt-get -y install dotnet-dev-1.0.4
