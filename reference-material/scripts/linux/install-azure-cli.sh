8a11e9f36d45cec590eeafdee78e5abc7586c9b7
Razi Rais <razibinrais@outlook.com>
2017-07-10 16:49:59 -0400

    Update install-azure-cli.sh

--- reference-material/scripts/linux/install-azure-cli.sh
+++ reference-material/scripts/linux/install-azure-cli.sh
@@ -4,5 +4,5 @@
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+sudo apt-get -y install apt-transport-https
+sudo apt-get -y update && sudo apt-get install azure-cli
