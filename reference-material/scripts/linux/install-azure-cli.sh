9a0815d30b02f0b38234a14a65bdcc5b7f8fe6bc
Razi Rais <razibinrais@outlook.com>
2017-07-10 16:42:35 -0400

    Create install-azure-cli.sh

---
+++ reference-material/scripts/linux/install-azure-cli.sh
@@ -0,0 +1,8 @@
+#install azure cli 2.0
+#https://docs.microsoft.com/en-us/cli/azure/install-azure-cli#apt-get-for-debianubuntu
+
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
