5b57b7640034eaa6ba442cb6c06fa1df0e11c6a0
Razi Rais <razibinrais@outlook.com>
2017-07-10 17:15:37 -0400

    Update install.sh

--- reference-material/scripts/linux/install.sh
+++ reference-material/scripts/linux/install.sh
@@ -1,9 +1,7 @@
 sudo mkdir install-scripts
 cd install-scripts
 
-#un-comment commands related to docker if you're not using azure ubuntu 16.04 vm image that comes with docker installed on it.
-
-#sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-docker.sh
+sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-docker.sh
 sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-nodejs.sh
 sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-yeoman.sh
 sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-dotnetcore.sh
@@ -11,7 +9,7 @@ sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/re
 sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-azure-cli.sh
 sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/scripts/linux/install-bower.sh
 
-#sudo chmod +x $PWD/install-docker.sh
+sudo chmod +x $PWD/install-docker.sh
 sudo chmod +x $PWD/install-nodejs.sh
 sudo chmod +x $PWD/install-yeoman.sh
 sudo chmod +x $PWD/install-dotnetcore.sh
@@ -19,7 +17,7 @@ sudo chmod +x $PWD/install-git.sh
 sudo chmod +x $PWD/install-azure-cli.sh
 sudo chmod +x $PWD/install-bower.sh
 
-#./install-docker.sh 
+./install-docker.sh 
 ./install-nodejs.sh
 ./install-yeoman.sh 
 ./install-dotnetcore.sh
