d7be3552a0b2dda32ea3edc8e0d61c0b8e86c752
Razi Rais <razibinrais@outlook.com>
2017-12-25 21:10:55 -0500

    Update install-minikube-ubuntu16.04LTS.sh

--- reference-material/install-minikube-ubuntu16.04LTS.sh
+++ reference-material/install-minikube-ubuntu16.04LTS.sh
@@ -1,6 +1,7 @@
 ##### Usage
 # sudo curl -O https://raw.githubusercontent.com/razi-rais/microservices/master/reference-material/install-minikube-ubuntu16.04LTS.sh
 # sudo chmod +x $PWD/install-minikube-ubuntu16.04LTS.sh
+# ./install-minikube-ubuntu16.04LTS.sh 
 
 
 sudo apt-get -y update 
